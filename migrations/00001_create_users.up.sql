d7daa31b54fc2c30ec62c7679cd2e30e36228c40
Johnhi19 <johannes.hierlinger@gmail.com>
2026-01-10 22:41:13 +0100

    Change email column to be created in first startup

--- migrations/00001_create_users.up.sql
+++ migrations/00001_create_users.up.sql
@@ -2,5 +2,6 @@ CREATE TABLE IF NOT EXISTS `users` (
     `id` int NOT NULL AUTO_INCREMENT,
     `username` varchar(50) UNIQUE NOT NULL,
     `password` varchar(255) NOT NULL,
+    `email` VARCHAR(255) UNIQUE NOT NULL,
     PRIMARY KEY (`id`)
 ) ENGINE = InnoDB DEFAULT CHARSET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci
